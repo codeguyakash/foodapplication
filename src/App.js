25a69f6291c4fdf664cdb841e68c8d5ab6a517de
Akash <codeguyakash@gmail.com>
2022-12-16 10:54:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+
+import React from "react"
+import Resturant from "./component/Basics/Resturant";
+
+const App = () => {
+    return <Resturant/>;
+};
+
+export default App;
+
+
+
+
+// import React from 'react' (no need to import react from React after version 17)
+
+// import React from "react"
+
+// const App = () => {
+//     return (
+//         <>
+//             <MyName/>
+//             <h1 className="myclassname">Hello Akash !</h1>
+//             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+//             </p>
+//         </>
+//     );
+// };
+// const MyName = () => {
+//     return <h1>Welcome</h1>
+// };
+
+
+
+// const App = () => {
+//     return (
+//         <div>
+//             <h1>welcome to React First Project by Codeguyakash
+//             </h1></div>
+//     )
+// }
+
+// const App = () => {
+//     return React.createElement(
+//         "div",
+//         {},
+//         React.createElement("h1", {}, "Hello World!")
+//     );
+// };
+
+
+// export default App
