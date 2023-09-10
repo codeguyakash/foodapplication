7a4dd072a1785c331e839823bfbd337feac65459
Akash <codeguyakash@gmail.com>
2023-09-10 23:22:30 +0530

    add images

--- src/component/Basics/menuApi.js
+++ src/component/Basics/menuApi.js
@@ -1,7 +1,7 @@
 const Menu = [
     {
       id: 1,
-      image: "images/maggi.jpg",
+      image: "https://foodtasted.org/wp-content/uploads/2019/06/Egg-Maggi.jpg",
       name: "maggi",
       category: "breakfast",
       price: "12₹",
@@ -11,7 +11,7 @@ const Menu = [
   
     {
       id: 2,
-      image: "images/allupakoida.jpg",
+      image: "https://www.cookforindia.com/wp-content/uploads/2016/03/aloo-pakora.jpg",
       name: "allu pakoda",
       category: "evening",
       price: "20₹",
@@ -20,7 +20,7 @@ const Menu = [
     },
     {
       id: 3,
-      image: "images/corn.jpg",
+      image: "https://www.allrecipes.com/thmb/jPx5C_UjGl062_eppi3DHnR8Cus=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/9202155-8b6188347d064594ada9134869af1a31.jpg",
       name: "corn",
       category: "breakfast",
       price: "10₹",
@@ -29,7 +29,7 @@ const Menu = [
     },
     {
       id: 4,
-      image: "../images/chola.jpg",
+      image: "https://i.pinimg.com/736x/e3/5e/52/e35e5233bfb52e2c504c07f8db750342.jpg",
       name: "chola",
       category: "lunch",
       price: "50₹",
@@ -38,7 +38,7 @@ const Menu = [
     },
     {
       id: 5,
-      image: "../images/pizza.jpg",
+      image: "https://hips.hearstapps.com/hmg-prod/images/classic-cheese-pizza-recipe-2-64429a0cb408b.jpg?crop=0.8888888888888888xw:1xh;center,top&resize=1200:*",
       name: "pizza",
       category: "evening",
       price: "80₹",
@@ -47,7 +47,7 @@ const Menu = [
     },
     {
       id: 6,
-      image: "../images/nonvegthali.jpg",
+      image: "https://i.ytimg.com/vi/QCRdZi63QhY/maxresdefault.jpg",
       name: "Non-Veg Thali",
       category: "dinner",
       price: "180₹",
@@ -56,7 +56,7 @@ const Menu = [
     },
     {
       id: 7,
-      image: "../images/sweet.jpg",
+      image: "https://www.vikhrolicucina.com/uploads/stories/1662465216_groupindianassortedsweetsmithaiwithdiya.jpg",
       name: "Sweets",
       category: "dinner",
       price: "60₹",
@@ -65,7 +65,7 @@ const Menu = [
     },
     {
       id: 8,
-      image: "../images/rajmarice.jpg",
+      image: "https://img.buzzfeed.com/tasty-app-user-assets-prod-us-east-1/recipes/17a928443e754d54a4c0a125bc2b9c74.jpeg?resize=1200:*&output-format=jpg&output-quality=auto",
       name: "Rajma Rice",
       category: "lunch",
       price: "60₹",
@@ -74,7 +74,7 @@ const Menu = [
     },
     {
       id: 9,
-      image: "../images/samosa.jpg",
+      image: "https://b.zmtcdn.com/data/pictures/chains/5/18463985/7f79ce04901ec0c63f0d4cd63a31284b.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
       name: "samaso",
       category: "evening",
       price: "10₹",
@@ -83,7 +83,7 @@ const Menu = [
     },
     {
       id: 10,
-      image: "../images/samosa.jpg",
+      image: "https://i.ndtvimg.com/i/2017-08/kaju-barfi_650x400_71501923806.jpg",
       name: "Kaju Katli",
       category: "Sweets",
       price: "10₹",
@@ -92,8 +92,8 @@ const Menu = [
     },
     {
       id: 11,
-      image: "../images/samosa.jpg",
-      name: "Wine",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSf4LhnbtVoS8yNtopHvm07ECYP7EXw4eTo3A&usqp=CAU",
+      name: "Mango Shake",
       category: "Drink",
       price: "10₹",
       description:
